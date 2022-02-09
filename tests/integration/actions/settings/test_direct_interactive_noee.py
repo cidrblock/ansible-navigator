181b494614e1f74c43fe251ae3cc29f511092001
Ali <alhart@redhat.com>
2022-02-09 16:27:27 -0500

    Refining tests for direct interactive ee and no ee tests

---
+++ tests/integration/actions/settings/test_direct_interactive_noee.py
@@ -0,0 +1,30 @@
+"""Tests for ``config`` from CLI, interactive, without an EE.
+"""
+import pytest
+
+from ..._interactions import Command
+from ..._interactions import Step
+from ..._interactions import add_indices
+from ..._interactions import step_id
+from .base import BaseClass
+from .base import base_steps
+
+
+CLI = Command(subcommand="settings", execution_environment=False).join()
+
+initial_steps = (
+    Step(
+        user_input=CLI,
+        comment="ansible-navigator settings command top window",
+        look_fors=["ansible_runner_artifact_dir", "help_playbook"],
+    ),
+)
+
+steps = add_indices(initial_steps + base_steps)
+
+
+@pytest.mark.parametrize("step", steps, ids=step_id)
+class Test(BaseClass):
+    """Run the tests for ``settings`` from CLI, interactive, without an EE."""
+
+    UPDATE_FIXTURES = True
