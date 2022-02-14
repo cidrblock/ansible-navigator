0859b0c2fa6ce936101a9aae5c75f4423b78ce14
Jake Jackson <jljacks93@gmail.com>
2022-02-14 12:30:03 -0500

    test fixes and unit tests

--- tests/integration/actions/settings/test_direct_interactive_noee.py
+++ tests/integration/actions/settings/test_direct_interactive_noee.py
@@ -3,7 +3,7 @@
 import pytest
 
 from ..._interactions import Command
-from ..._interactions import Step
+from ..._interactions import UiTestStep
 from ..._interactions import add_indices
 from ..._interactions import step_id
 from .base import BaseClass
@@ -13,10 +13,10 @@ from .base import base_steps
 CLI = Command(subcommand="settings", execution_environment=False).join()
 
 initial_steps = (
-    Step(
+    UiTestStep(
         user_input=CLI,
         comment="ansible-navigator settings command top window",
-        look_fors=["ansible_runner_artifact_dir", "help_playbook"],
+        present=["ansible_runner_artifact_dir", "help_playbook"],
     ),
 )
 
@@ -27,4 +27,4 @@ steps = add_indices(initial_steps + base_steps)
 class Test(BaseClass):
     """Run the tests for ``settings`` from CLI, interactive, without an EE."""
 
-    UPDATE_FIXTURES = False
+    UPDATE_FIXTURES = True
--- tests/integration/actions/settings/test_welcome_interactive_noee.py
+++ tests/integration/actions/settings/test_welcome_interactive_noee.py
@@ -3,7 +3,7 @@
 import pytest
 
 from ..._interactions import Command
-from ..._interactions import Step
+from ..._interactions import UiTestStep
 from ..._interactions import add_indices
 from ..._interactions import step_id
 from .base import BaseClass
@@ -13,11 +13,11 @@ from .base import base_steps
 CLI = Command(execution_environment=False).join()
 
 initial_steps = (
-    Step(user_input=CLI, comment="welcome screen"),
-    Step(
+    UiTestStep(user_input=CLI, comment="welcome screen"),
+    UiTestStep(
         user_input=":settings",
         comment="enter settings from welcome screen",
-        look_fors=["ansible_runner_artifact_dir", "help_playbook"],
+        present=["ansible_runner_artifact_dir", "help_playbook"],
     ),
 )
 
@@ -28,4 +28,4 @@ steps = add_indices(initial_steps + base_steps)
 class Test(BaseClass):
     """Run the tests for ``settings`` from CLI, interactive, without an EE."""
 
-    UPDATE_FIXTURES = False
+    UPDATE_FIXTURES = True
